dc1cc9906513b76d418a126b2981c67aa072aa00
Seunghun Lee <waydi1@gmail.com>
2015-06-18 07:55:58 +0900

    Add helper function pext_msg
    Signed-off-by: Seunghun Lee <waydi1@gmail.com>

--- pext.c
+++ pext.c
@@ -1,6 +1,22 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 
+void pext_msg(struct super_block *sb, const char *prefix,
+		const char *fmt, ...)
+{
+	struct va_format vaf;
+	va_list args;
+
+	va_start(args, fmt);
+
+	vaf.fmt = fmt;
+	vaf.va = &args;
+
+	printk("%sPEXT-fs (%s): %pV\n", prefix, sb->s_id, &vaf);
+
+	va_end(args);
+}
+
 static int pext_fill_super(struct super_block *sb, void *data, int silent)
 {
 	return -1;
